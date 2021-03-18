4176205c24ac5368c0c270b38166edd3a3f8acaa
Emre Buyuran <emre.byrn@gmail.com>
2021-03-18 03:03:34 +0300

    git init and files added

---
+++ public/chat.js
@@ -0,0 +1,28 @@
+const socket = io.connect('http://localhost:3000')
+
+const sender = document.getElementById('sender')
+const message = document.getElementById('message')
+const submitButton = document.getElementById('submitButton')
+const output = document.getElementById('output')
+const feedback = document.getElementById('feedback')
+
+submitButton.addEventListener('click', () =>{
+  socket.emit('chat',{
+    message:message.value,
+    sender: sender.value
+  })
+})
+
+socket.on('chat', data =>{
+  feedback.innerHTML = '';
+  output.innerHTML += '<p><strong>' + data.sender + ' : </strong>' + data.message + '</p>'
+  message.value = '';
+})
+
+message.addEventListener('keypress', () =>{
+  socket.emit('typing',sender.value)
+})
+
+socket.on('typing', data => {
+  feedback.innerHTML = '<p>' + data +' yazıyor...</p>'
+})
